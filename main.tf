6e461dcdc86fc06e614701df249f0385c3ecec58
kboid <sageerkubes17@gmail.com>
2025-11-02 22:45:45 +0530

    added required creds to the user

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  #public_key = var.public_key # Adjust path to your public SSH key
+  #key_name   = var.key_name
+  public_key = var.public_key # Adjust path to your public SSH key
 }
 
 resource "aws_security_group" "nginx_sg" {
