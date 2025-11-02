ae0885c2f931177a2a485669fd5f1316cd1c4007
kboid <sageerkubes17@gmail.com>
2025-11-02 21:52:19 +0530

    added required creds to the user

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "key_name" {
-  description = "Name of the AWS key pair"
-  default     = "practise"
-}
-
 variable "public_key_path" {
   description = ""C:\Users\Admin\Desktop\practise.pub""
   default     = "~/.ssh/id_rsa.pub"
